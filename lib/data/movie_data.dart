327385b268d747d583c57779e3af5ad9ac313449
sangiba <sangiba@baleomol.dev>
2024-02-21 09:34:50 +0700

    Make summary only 100 max

--- lib/data/movie_data.dart
+++ lib/data/movie_data.dart
@@ -7,7 +7,7 @@ List<Movie> moviesData = [
     director: 'Frank Darabont',
     year: 1994,
     summary:
-        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+        'Two imprisoned men bond over a number of years, finding solace and eventual redemption.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -25,7 +25,7 @@ List<Movie> moviesData = [
     director: 'Christopher Nolan',
     year: 2008,
     summary:
-        'When the menace known as The Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+        'Batman faces The Joker in a test of his ability to fight injustice in Gotham.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -34,7 +34,7 @@ List<Movie> moviesData = [
     director: 'Sidney Lumet',
     year: 1957,
     summary:
-        'A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.',
+        'A jury holdout attempts to prevent a miscarriage of justice.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -43,7 +43,7 @@ List<Movie> moviesData = [
     director: 'Steven Spielberg',
     year: 1993,
     summary:
-        'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
+        'Industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce during WWII.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -52,7 +52,7 @@ List<Movie> moviesData = [
     director: 'Peter Jackson',
     year: 2003,
     summary:
-        'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.',
+        'Gandalf leads the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -61,7 +61,7 @@ List<Movie> moviesData = [
     director: 'Quentin Tarantino',
     year: 1994,
     summary:
-        'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+        'The lives of mob hitmen, a boxer, a gangster, and others intertwine in tales of violence and redemption.',
     genres: ['Action', 'Sci-Fi'],
   ),
   Movie(
@@ -70,7 +70,7 @@ List<Movie> moviesData = [
     director: 'Robert Zemeckis',
     year: 1994,
     summary:
-        'The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.',
+        'The life of an Alabama man unfolds against the backdrop of historical events.',
     genres: ['Action', 'Sci-Fi'],
   ),
 ];
